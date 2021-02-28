5b12b2ccde756332ad216abf586f821e8549df35
zuohong <823178351@qq.com>
2021-02-28 11:44:47 +0800

    first commit

---
+++ src/main/java/com/hbswjj/editorfile/EditorfileApplication.java
@@ -0,0 +1,13 @@
+package com.hbswjj.editorfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditorfileApplication.class, args);
+    }
+
+}
